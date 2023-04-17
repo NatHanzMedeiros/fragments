b8300a27f8c924feb5d30d9ea39fabf0d67adaae
nathan_cristiano <nathanmedeirosc@gmail.com>
2023-04-17 09:17:19 -0300

    start commit

---
+++ app/src/main/java/com/example/tab/Adaptador.java
@@ -0,0 +1,34 @@
+package com.example.tab;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class Adaptador extends FragmentStateAdapter {
+    public Adaptador(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position){
+            case 1 :
+            return  new segundo();
+
+        case 2 :
+            return new terceiro();
+
+            case 3 :
+                return new fragment_quarto();
+    }
+        return new primeiro();
+    }
+
+    @Override
+    public int getItemCount() {
+        return 4;
+    }
+}
